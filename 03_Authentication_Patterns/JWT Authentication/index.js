ead99ae21907893bef41705317dabef8e2490255
LGoat <issimo1122@gmail.com>
2026-04-01 19:51:12 +0100

    Learn: Login Route With JWT and Bcrypt

--- 03_Authentication_Patterns/JWT Authentication/index.js
+++ 03_Authentication_Patterns/JWT Authentication/index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express()
 const bcrypt = require("bcrypt")
+const jwt = require('jsonwebtoken')
 const Users = require('./database/utilisateur')
 
 app.use(express.json())
@@ -19,13 +20,42 @@ app.post('/register', async (req, res) => {
             const newUser = new Users({
                 username,
                 email,
-                pwd_hashed
+                password: pwd_hashed
             })
             newUser.save()
                 .then(user => res.status(200).json(user))
                 .catch(err => res.status(500).json(err))
         })
     }
+})
+
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body
+    const user = await Users.findOne({ email })
+    if(!user){
+        return res.status(404).json({ message: 'User Not Found !' })
+    }
+    bcrypt.compare(password, user.password)
+        .then(result => {
+            if(!result){
+                return res.status(400).json({ message: 'Password Incorrect !' })
+            }
+            const data = { email: email, username: user.username }
+            jwt.sign(
+                data,
+                "RANDOM_TOKEN_SECRET",
+                { expiresIn: "2h" },
+                (error, token) => {
+                    if(error){
+                        res.status(404).json({ message: error.message })
+                    }
+                    return res.status(200).json({ token: token, result: result })
+                }
+            )
+        })
+        .catch(error => {
+            return res.status(500).json({message: error.message})
+        })
 })
 
 app.listen(4000, () => console.log('Start Server !'))
