0e1b303c0240d1c03f38dc54769f78732a790fc7
LGoat <issimo1122@gmail.com>
2026-04-01 15:40:05 +0100

    Learn: Register Route With Bcrypt

--- 03_Authentication_Patterns/JWT Authentication/index.js
+++ 03_Authentication_Patterns/JWT Authentication/index.js
@@ -29,6 +29,7 @@ app.post('/register', async (req, res) => {
     }
 })
 
+
 app.post('/login', async (req, res) => {
     const { email, password } = req.body
     const user = await Users.findOne({ email })
